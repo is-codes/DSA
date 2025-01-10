6926635fc2123209f0485fbf671fdd6bd64e9162
is-codes <isharathore0000@gmail.com>
2025-01-10 23:13:31 +0530

    code

---
+++ binarysearch.java
@@ -0,0 +1,17 @@
+package DSA;
+
+public class binarysearch {
+    
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n-1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+}
