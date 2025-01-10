47a2f52ba7c6f4327523f10ec9f0b6406216ff24
is-codes <isharathore0000@gmail.com>
2025-01-10 22:49:30 +0530

    number of odd and even in array

---
+++ countoddeven.java
@@ -0,0 +1,25 @@
+package DSA;
+
+public class countoddeven {
+    
+       public static int[] countOddEven(int[] arr) {
+           int countOdd = 0, countEven = 0;
+           for (int i = 0; i < arr.length; i++) {
+              // checking if the element is even
+              if (arr[i] % 2 == 0) {
+                 countEven++;
+              } else {
+                 // if not even, it must be odd
+                 countOdd++;
+              }
+           }
+           return new int[] {countOdd, countEven};
+        }
+     
+        public static void main(String[] args) {
+           int[] arr = {2, 3, 4, 5, 6};
+           int[] ans = countOddEven(arr);
+           System.out.println(ans[0] + " " + ans[1]);
+        }
+     }
+
