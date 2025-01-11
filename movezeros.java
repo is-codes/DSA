280e1223bb48c763641687ce10ea33fad6d9fea8
is-codes <isharathore0000@gmail.com>
2025-01-11 19:42:18 +0530

    code

---
+++ movezeros.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class movezeros {
+    
+        public void moveZeroes(int[] nums) {
+            int left = 0;
+    
+            for (int right = 0; right < nums.length; right++) {
+                if (nums[right] != 0) {
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                    left++;
+                }
+            }        
+        }
+    }
+
