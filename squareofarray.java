199a4c725f274fa15eb3c4b7d0f13d1264ed787e
is-codes <isharathore0000@gmail.com>
2025-01-11 22:20:23 +0530

    squares of sorted array

---
+++ squareofarray.java
@@ -0,0 +1,17 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class squareofarray {
+    
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
+
