19181729559d1362cdb3ca96d31407eda1c129f3
is-codes <isharathore0000@gmail.com>
2025-01-11 17:49:31 +0530

    Check if string is pangram

---
+++ pangram.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class pangram {
+    
+        public boolean checkIfPangram(String sentence) {
+            if(sentence.length()<26) return false;
+            for(char ch='a';ch<='z';ch++){
+                if(sentence.indexOf(ch)<0){
+                    return false;
+                }
+             }
+            return true;
+        }
+    }
+    
+
