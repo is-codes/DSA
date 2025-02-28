8cf16d0ccd86171115ae73c06f8e24f2889d21a9
is-codes <isharathore0000@gmail.com>
2025-02-28 17:38:45 +0530

    code

---
+++ arraypartition.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class arraypartition {
+    
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
